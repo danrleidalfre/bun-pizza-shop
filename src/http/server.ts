a4796abdd5dbd647224afacee915d40c680b0342
Danrlei <danrleidalfre@gmail.com>
2024-04-09 19:14:00 -0300

    add auth to click link email

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,12 @@
 import Elysia from "elysia";
 import {sendAuthLink} from './routes/send-auth-link'
 import {registerRestaurant} from "./routes/register-restaurant.ts";
+import {authenticateFromLink} from "./routes/authenticate-from-link.ts";
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('HTTP Server Running...')
